4dca9c471990a3086fbf87fc5f1ee111528db5d7
alan <alanarostegui@hotmail.com>
2022-05-22 19:46:03 -0500

    feat: create match url to route fn

--- router.js
+++ router.js
@@ -3,11 +3,24 @@ class Router {
     this.routes = routes;
     this._loadInitialRoute();
   }
-  
+
   _loadInitialRoute() {
     const pathNameSplit = window.location.pathname.split("/");
     const pathSegments = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : "";
 
     this.loadRoute(...pathSegments);
+  }
+
+  _matchUrlToRoute(urlSegments) {
+    const matchedRoute = this.routes.find((route) => {
+      const routePathSegments = route.path.split("/").slice(1);
+      if (routePathSegs.length !== urlSegments.length) {
+        return false;
+      }
+      return routePathSegments.every((routePathSegment, index) => {
+        return routePathSegment === urlSegments[index];
+      });
+    });
+    return matchedRoute;
   }
 }
